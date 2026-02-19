c7c60c29bd2e48b9d984006f53c90e50af4c4914
Anirudh Panwar <anirudh0976@gmail.com>
2026-02-19 22:26:59 +0530

    IMplemented the functionality to add the onboarding status of the user to its session

--- auth.ts
+++ auth.ts
@@ -74,6 +74,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
 
             token.role = existingUser.role;
             token.isAppAdmin = existingUser.isAppAdmin;
+            token.hasCompletedOnboarding = existingUser.hasCompletedOnboarding;
 
             return token;
         },
