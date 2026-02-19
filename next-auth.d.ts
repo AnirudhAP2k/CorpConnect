8e910745f572c2ffce4e353ee1d1f2f5da2e4bfe
Anirudh Panwar <anirudh0976@gmail.com>
2026-02-19 20:55:14 +0530

    Minor fixes in the User type

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -2,12 +2,13 @@ import { UserRole } from "@prisma/client"
 import NextAuth, { type DefaultSession } from "next-auth"
 
 export type ExtendedUser = DefaultSession["user"] & {
-    role: UserRole
+    role: UserRole;
+    isAppAdmin: boolean;
 }
 
 declare module "next-auth" {
     interface Session {
         id: string
         user: ExtendedUser
-    }  
+    }
 }
