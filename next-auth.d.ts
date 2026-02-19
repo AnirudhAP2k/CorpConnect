35121944c18a4d448e26ed4f0068833de2e2a7bc
Anirudh Panwar <anirudh0976@gmail.com>
2026-02-19 22:27:50 +0530

    Updated the auth config types

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -4,6 +4,7 @@ import NextAuth, { type DefaultSession } from "next-auth"
 export type ExtendedUser = DefaultSession["user"] & {
     role: UserRole;
     isAppAdmin: boolean;
+    hasCompletedOnboarding: boolean;
 }
 
 declare module "next-auth" {
