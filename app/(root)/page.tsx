a306dcbe110bca929787a67d6cf9e227cb447755
Anirudh Panwar <anirudh0976@gmail.com>
2026-02-20 22:38:49 +0530

    Minor fixes in the home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -11,7 +11,7 @@ import { Suspense, useCallback, useEffect, useState } from "react";
 const Home = () => {
     const [events, setEvents] = useState([]);
     const [totalPages, setTotalPages] = useState(0);
-    
+
     const getAllEvents = useCallback(async () => {
         const params = {
             query: '',
@@ -20,22 +20,19 @@ const Home = () => {
             page: 1,
         }
 
-        await axios.get("/api/events/all", {
+        await axios.get("/api/events", {
             params,
-            headers: {
-            'Content-Type': 'application/json',
-            },
         })
-        .then((response) => {
-            console.log(response);
-            
-            setEvents(response.data.data);
-            setTotalPages(response.data.totalPages);
-        })
-        .catch((error) => {
-            const errMessage = error.response?.data?.error || error.message;
-            console.error(errMessage);
-        });
+            .then((response) => {
+                console.log(response);
+
+                setEvents(response.data.data);
+                setTotalPages(response.data.totalPages);
+            })
+            .catch((error) => {
+                const errMessage = error.response?.data?.error || error.message;
+                console.error(errMessage);
+            });
     }, []);
 
     useEffect(() => {
@@ -44,48 +41,48 @@ const Home = () => {
 
     return (
         <>
-        <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
-            <div className="wrapper grid grid-cols-1 md:grid-cols-2 2xl:gap-0 gap-5">
-            <div className="flex flex-col justify-center gap-8">
-                <h1 className="h1-bold">Host, Connect, Celebrate: Your Events, Our Platform</h1>
-                <p className="p-regular-20 md:p-regulat-24">
-                Book and learn helpful tips from 3,168+ mentiors in world-class companies with our global community.
-                </p>
-                <Button size="lg" asChild className="button w-full sm:w-fit">
-                <Link href="#events">
-                    Explore Now
-                </Link>
-                </Button>
-            </div>
+            <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
+                <div className="wrapper grid grid-cols-1 md:grid-cols-2 2xl:gap-0 gap-5">
+                    <div className="flex flex-col justify-center gap-8">
+                        <h1 className="h1-bold">Host, Connect, Celebrate: Your Events, Our Platform</h1>
+                        <p className="p-regular-20 md:p-regulat-24">
+                            Book and learn helpful tips from 3,168+ mentiors in world-class companies with our global community.
+                        </p>
+                        <Button size="lg" asChild className="button w-full sm:w-fit">
+                            <Link href="#events">
+                                Explore Now
+                            </Link>
+                        </Button>
+                    </div>
 
-            <Image
-                src="/assets/images/hero.png"
-                alt="hero"
-                width={1000}
-                height={1000}
-                className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
-            />
-            </div>
-        </section>
+                    <Image
+                        src="/assets/images/hero.png"
+                        alt="hero"
+                        width={1000}
+                        height={1000}
+                        className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
+                    />
+                </div>
+            </section>
 
-        <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
-            <h2 className="h2-bold">Trusted by <br /> Thousands of events</h2>
+            <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+                <h2 className="h2-bold">Trusted by <br /> Thousands of events</h2>
 
-            <div className="flex flex-col w-full gap-5 md:flex-row">
-            Search
-            CategoryFilter
-            </div>
+                <div className="flex flex-col w-full gap-5 md:flex-row">
+                    Search
+                    CategoryFilter
+                </div>
 
-            <Collection
-                data={events}
-                emptyTitle="No events found"
-                emptyStateSubtext="Come back later"
-                collectionType="All_events"
-                limit={6}
-                page={1}
-                totalPages={totalPages}
-            />
-        </section>
+                <Collection
+                    data={events}
+                    emptyTitle="No events found"
+                    emptyStateSubtext="Come back later"
+                    collectionType="All_events"
+                    limit={6}
+                    page={1}
+                    totalPages={totalPages}
+                />
+            </section>
         </>
     );
 }
