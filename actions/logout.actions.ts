d6e779d7282fdad2b2345f51a1593a8c170a1173
Anirudh Panwar <anirudh0976@gmail.com>
2026-03-26 19:28:38 +0530

    Refactored the logout server action

--- actions/logout.actions.ts
+++ actions/logout.actions.ts
@@ -1,24 +1,11 @@
 /**
  * Logout action
- * Revoke refresh token
- * Delete refresh token cookie
- * Sign out
+ * Sign out (triggers event to revoke token in DB)
  */
 "use server";
 
 import { signOut } from "@/auth";
-import { revokeToken } from "@/lib/tokens";
-import { cookies } from "next/headers";
 
 export const logout = async () => {
-    const cookieStore = await cookies();
-    const refreshToken = cookieStore.get("refreshToken")?.value;
-
-    if (refreshToken) {
-        await revokeToken(refreshToken);
-    }
-
-    cookieStore.delete("refreshToken");
-
     await signOut({ redirectTo: "/" });
 }
