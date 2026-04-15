7f4eae3beda91181bc3aec7b9fb6459b54315d9c
Anirudh Panwar <anirudh0976@gmail.com>
2026-04-15 19:47:42 +0530

    Developed the getUserTierWithActiveOrg helper funciton

--- data/user.ts
+++ data/user.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { prisma } from "@/lib/db";
+import { User } from "next-auth";
 
 export const getUserById = async (id: string) => {
     try {
@@ -21,3 +22,19 @@ export const getUserByEmail = async (email: string) => {
         return null;
     }
 };
+
+export const getUserTierWithActiveOrg = async (user: User) => {
+    try {
+        if (!user.activeOrganizationId) {
+            return "FREE";
+        }
+
+        const apiCred = await prisma.apiCredential.findUnique({
+            where: { organizationId: user.activeOrganizationId }
+        });
+
+        return apiCred?.tier || "FREE";
+    } catch (error) {
+        return "FREE";
+    }
+}
