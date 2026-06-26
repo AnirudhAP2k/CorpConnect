b99abc74e40f67a3398eae751fa42b1ee25dd707
Anirudh Panwar <anirudh0976@gmail.com>
2026-06-26 20:51:01 +0530

    feat: add Edge runtime Sentry initialization config

---
+++ sentry.edge.config.ts
@@ -0,0 +1,15 @@
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 0.1,
+
+  debug: true,
+
+  // send console.log, console.warn, and console.error calls as logs to Sentry
+  integrations: [
+    Sentry.consoleLoggingIntegration({ levels: ["log", "warn", "error"] }),
+  ],
+});
