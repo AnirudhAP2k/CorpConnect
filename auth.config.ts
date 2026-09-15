6ac93653d905ea41a4fae0e3f60088928927b704
Anirudh Panwar <anirudh0976@gmail.com>
2026-09-15 21:44:34 +0530

    fix(auth): format auth configuration and types

--- auth.config.ts
+++ auth.config.ts
@@ -1,36 +1,39 @@
-import GitHub from "next-auth/providers/github"
+import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
-import type { NextAuthConfig } from "next-auth"
+import type { NextAuthConfig } from "next-auth";
 import { LoginSchema } from "@/lib/auth-validation";
 import { mapTokenToSession } from "@/auth.session";
 import { JWT_MAX_AGE_SECONDS } from "@/constants";
 
 export default {
-    providers: [
-        Google({
-            clientId: process.env.AUTH_GOOGLE_ID || "",
-            clientSecret: process.env.AUTH_GOOGLE_SECRET || "",
-        }),
-        GitHub({
-            clientId: process.env.AUTH_GITHUB_ID || "",
-            clientSecret: process.env.AUTH_GITHUB_SECRET || "",
-        }),
-        CredentialsProvider({
-            async authorize(credentials: any) {
-                const validated = LoginSchema.safeParse(credentials);
-                if (!validated.success) return null;
+	providers: [
+		Google({
+			clientId: process.env.AUTH_GOOGLE_ID || "",
+			clientSecret: process.env.AUTH_GOOGLE_SECRET || "",
+		}),
+		GitHub({
+			clientId: process.env.AUTH_GITHUB_ID || "",
+			clientSecret: process.env.AUTH_GITHUB_SECRET || "",
+		}),
+		CredentialsProvider({
+			async authorize(credentials: any) {
+				const validated = LoginSchema.safeParse(credentials);
+				if (!validated.success) return null;
 
-                return { email: validated.data.email, password: validated.data.password } as Record<string, string>;
-            }
-        }),
-    ],
-    session: {
-        maxAge: JWT_MAX_AGE_SECONDS,
-    },
-    callbacks: {
-        async session({ session, token }) {
-            return mapTokenToSession(session, token);
-        },
-    },
-} satisfies NextAuthConfig
+				return {
+					email: validated.data.email,
+					password: validated.data.password,
+				} as Record<string, string>;
+			},
+		}),
+	],
+	session: {
+		maxAge: JWT_MAX_AGE_SECONDS,
+	},
+	callbacks: {
+		async session({ session, token }) {
+			return mapTokenToSession(session, token);
+		},
+	},
+} satisfies NextAuthConfig;
